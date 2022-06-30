1e7d564a7601709b5c5af4d095cc058afd858c1f
sujit-h-singh <88031519+sujit-h-singh@users.noreply.github.com>
2022-06-30 16:13:22 +0530

    CH-276: dashboard page modified according to UX change (#18)
    * dashboard page modified according to UX change
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>
    
    * dashboard code fixes for node card
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>
    
    * design fixes as per review.
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>
    
    * test cases for health, nodes and bgactivity card
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>
    
    * Icons modified
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>
    
    * Info Icon added
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>
    
    * title info msg change
    
    Signed-off-by: Sujit Singh <sujit.h.singh@seagate.com>

--- manager/gui/src/utils/CommonUtilFunctions.ts
+++ manager/gui/src/utils/CommonUtilFunctions.ts
@@ -47,3 +47,22 @@ export function jsonTest(str: string) {
   }
   return true;
 }
+
+export function hexToRGB(h: string) {
+  let r: string | number = 0, g: string | number = 0, b: string | number = 0;
+
+  // 3 digits
+  if (h.length == 4) {
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
+
+    // 6 digits
+  } else if (h.length == 7) {
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+  }
+
+  return "rgb(" + +r + ", " + +g + ", " + +b + ")";
+}
