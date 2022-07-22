55b31c3d80029ff48af9bc371b53ec7b3d9dd8f8
Akash Dudhane <akash.a.dudhane@seagate.com>
2022-07-22 13:07:53 +0530

    CH-260 : Manager BE: Foundation: DB Store Framework (#21)
    * CH-260 : Manager BE: Foundation: DB Store Framework
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * DBAdmin & Exception class
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * CH-260 : DBAdmin & Exception class
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * CH-260 : Additional commands for db Admin.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Create Time Series Bucket method.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Added test file.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Added test file.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Codacy fix.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Codacy check
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Use abstract names to method and variables.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Redefination of methods & fields.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Added License info.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Added __init__ in abstract classes.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    * Added __init__ in abstract classes.
    
    Signed-off-by: Akash Dudhane <akash.a.dudhane@seagate.com>
    
    Co-authored-by: ajaysrivas <66410865+ajaysrivas@users.noreply.github.com>

---
+++ common/db_store/admin_db.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com or cortx-questions@seagate.com.
+
+from enum import Enum
+import pymongo
+from abc import ABC, abstractmethod
+from common.db_store.error import DBError
+
+
+class DBAdmin(ABC):
+
+    def __init__(self, db_endpoint, db_name) -> None:
+        self._endpoint = db_endpoint
+        self._db_name = db_name
+
+    @abstractmethod
+    def create_index(self, data_store_name, index_key, **kwargs):
+        pass
+
+    @abstractmethod
+    def list_indexes(self, data_store_name, **kwargs):
+        pass
+
+    @abstractmethod
+    def drop_index(self, data_store_name, index_name, **kwargs):
+        pass
+
+    @abstractmethod
+    def open_connection(self):
+        pass
+
+    @abstractmethod
+    def close_connection(self):
+        pass
+
+    @abstractmethod
+    def create_data_store(self, data_store_name, data_store_type, **kwargs):
+        pass
+
+    @abstractmethod
+    def delete_data_store(self, data_store_name, data_store_type, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_data_stores(self):
+        pass
+
+
+class DATASTORE(Enum):
+    TIMESERIES = "timeseries"
+    ALERT = "alert"
+    CONFIG = "config"
+    PERF = "perf"
+
+
+class MongoDBAdmin(DBAdmin):
+
+    def __init__(self, db_endpoint: str, db_name: str) -> None:
+        """Initialize Data store connection.
+
+        Args:
+            db_endpoint (str): MongoDB server endpoints.
+                Ex. mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017
+            db_name (str): Name of database.
+        """
+        super().__init__(db_endpoint, db_name)
+        # TODO : Use singleton design pattern Or make sure every
+        # open connection should be closed after use.
+        self.open_connection()
+
+    def open_connection(self):
+        """Open data store connection.
+
+        Raises:
+            DBError: Unable to create connection.
+        """
+        try:
+            self._client = pymongo.MongoClient(self._endpoint)
+            self._db = self._client[self._db_name]
+        except Exception as e:
+            # TODO : Once logging enabled, uncomment all Log comments.
+            # Log.error(f"Unable to connect data store server endpoints \
+            #     {self._endpoint} {e}")
+            raise DBError(f"Unable to connect data store server endpoints \
+                          {self._endpoint} with Error : {e}")
+
+    def create_index(self, data_store_name: str, index_key: str, **kwargs):
+        """Create an index on this data store.
+
+        Args:
+            data_store_name (str): Name of data store.
+            index_key (str): Key/field on which index is created.
+
+        Raises:
+            DBError: Unable to create index.
+        """
+        try:
+            return self._db[data_store_name].create_index(index_key)
+        except Exception as e:
+            # Log.error(f"Unable to create Index. Error : {e}")
+            raise DBError(f"Index '{index_key}' creation failed. {e}")
+
+    def list_indexes(self, data_store_name: str, **kwargs):
+        """List all index present in data store.
+
+        Args:
+            data_store_name (str): Name of data store.
+
+        Returns:
+            list: List of indexes.
+        """
+        index_list = []
+        indexes = self._db[data_store_name].list_indexes()
+        for index in indexes:
+            index_list.append(index["name"])
+        return index_list
+
+    def drop_index(self, data_store_name: str, index_name: str, **kwargs):
+        """Drop index from data store.
+
+        Args:
+            data_store_name (str): Name of data store.
+            index_name (str): Index name.
+
+        Raises:
+            DBError: Unable to drop index.
+        """
+        if index_name in self.list_indexes(data_store_name):
+            self._db[data_store_name].drop_index(index_name)
+        else:
+            raise DBError(f"Invalid index key {index_name}.")
+
+    def close_connection(self):
+        """End all server sessions created by current client \
+            and disconnect from MongoDB."""
+        self._client.close()
+
+    def create_data_store(self, data_store_name: str,
+                          data_store_type: str = None, **kwargs):
+        """Create data store.
+
+        Args:
+            data_store (str): Type of data store.
+            data_store_name (str): Name of data store.
+            kwargs (_type_): keyword arguments.
+        """
+        if data_store_type == DATASTORE.TIMESERIES.value:
+            timeField = kwargs.get('timeField')
+            metaField = kwargs.get('metaField')
+            granularity = kwargs.get('granularity')
+            expire_time = kwargs.get('expire_time')
+            try:
+                if data_store_name not in self.get_data_stores():
+                    return self._db.create_collection(
+                        data_store_name,
+                        timeseries={
+                            'timeField': timeField,
+                            'metaField': metaField,
+                            'granularity': granularity},
+                        expireAfterSeconds=expire_time)
+                else:
+                    return f"Data store {data_store_name} already exists."
+            except Exception as e:
+                # Log.error(f"Unable to create TimeSeries data_store \
+                #     {data_store_name} Error : {e}")
+                raise DBError(f"Unable to create Time Series data store \
+                    {data_store_name}. Error : {e}")
+        else:
+            return self._db[data_store_name]
+
+    def delete_data_store(self, data_store_name: str,
+                          data_store_type: str = None, **kwargs):
+        """Remove data store.
+
+        Args:
+            data_store_name (str): Name of data store.
+            data_store_type (str): Type of data store.
+
+        Raises:
+            DBError: Invalid data store.
+        """
+        if data_store_name in self.get_data_stores():
+            self._db.drop_collection(data_store_name)
+        else:
+            raise DBError(f"Invalid data store {data_store_name}.")
+
+    def get_data_stores(self):
+        """Get list of data stores."""
+        return self._db.list_collection_names()
---
+++ common/test/test_datastore_backend.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com or cortx-questions@seagate.com.
+
+import pytest
+from datetime import datetime
+from common.db_store.db import MongoDB
+from common.db_store.admin_db import MongoDBAdmin
+
+
+pytestmark = pytest.mark.unit
+admin_db = None
+db = None
+
+
+@pytest.fixture
+def setup_admin_db():
+    global admin_db
+    admin_db = MongoDBAdmin(
+       db_endpoint="mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017",
+       db_name="test")
+    yield
+    admin_db.close_connection()
+
+
+@pytest.fixture
+def setup_db():
+    global db
+    db = MongoDB(
+       db_endpoint="mongodb://mongo-0.mongo,mongo-1.mongo,mongo-2.mongo:27017",
+       db_name="test")
+    yield
+    db.close()
+
+
+def test_create_time_series_data_store(setup_admin_db):
+    """Test by creating data store & listing it back."""
+    admin_db.create_data_store(
+        data_store_type="timeseries",
+        data_store_name="test_coll",
+        timeField="timestamp"
+        )
+    collection_list = admin_db.get_data_stores()
+    assert 'test_coll' in collection_list, \
+        "Failed to create timeseries data store."
+
+
+def test_create_index(setup_admin_db):
+    """Test by creating index & listing it back."""
+    admin_db.create_index("test_coll", "timestamp")
+    index_list = admin_db.list_indexes("test_coll")
+    assert 'timestamp_1' in index_list, "Failed to create index."
+
+
+def test_save_data(setup_db):
+    """Test by saving data and reading it back."""
+    record = {"timestamp": datetime.now(),
+              "comp": "disk", "measures": {'cpu': '23', 'memory': '235'}}
+    result = db.save_data("test_coll", data=record)
+    assert result is not None, "Failed to save Data."
+    get_list_of_records = db.get_data("test_coll")
+    assert any(r['measures'] == record['measures']
+               for r in get_list_of_records)
+
+
+def test_get_data(setup_db):
+    """Test by listing data."""
+    get_list_of_records = db.get_data("test_coll")
+    assert get_list_of_records is not None, "Failed to fetch Data."
+
+
+def test_delete_index(setup_admin_db):
+    """Test by deleting index and confirm by listing indexes."""
+    admin_db.drop_index("test_coll", "timestamp_1")
+    index_list = admin_db.list_indexes("test_coll")
+    assert 'timestamp_1' not in index_list, "Failed to delete index."
+
+
+def test_delete_collection(setup_admin_db):
+    """Test by deleting collection and confirm by listing collection."""
+    admin_db.delete_data_store("test_coll")
+    collection_list = admin_db.get_data_stores()
+    assert 'test_coll' not in collection_list, "Failed to delete collection."
