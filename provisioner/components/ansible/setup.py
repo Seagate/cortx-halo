9991177eca6326663b1ae9076c2ee8f5ab722ad0
Rajnish-TheGreat <rajnishkumar.mishra@seagate.com>
2022-08-16 05:44:41 -0600

    CH-330 Provisioning scripts for components: Install Halo Monitoring package and Ansible Added License and TODO
    Signed-off-by: Rajnish-TheGreat <rajnishkumar.mishra@seagate.com>

--- provisioner/components/ansible/setup.py
+++ provisioner/components/ansible/setup.py
@@ -1,4 +1,22 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com or cortx-questions@seagate.com.
+
 import os
 
 filename = "/opt/halo/install_depot/ansible.tar.gz"
@@ -8,7 +26,7 @@ def createFile():
     try:
         if os.system("ls %s" %(extract_path)) != 0:
             os.system("mkdir %s" %(extract_path))
-            print("Directory Created")
+            print("Directory Created %s" %extract_path)
     except Exception as e:
         print(f'{e}')
 
@@ -26,3 +44,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
--- provisioner/components/monitor/test_monitor.py
+++ provisioner/components/monitor/test_monitor.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com or cortx-questions@seagate.com.
+
 from setup import *
 from teardown import *
 from validate import *
@@ -6,7 +25,6 @@ from validate import *
 def test_folderCreation():
     createFile()
     x = os.system("ls /opt/halo/install_depot/monitor")
-    print(x)
     assert x==0, "File not created"
 
 def test_setup():
