373b157d4511469841ca42b142ff47c5f1f6c2d1
shaileshsvc <92013295+shaileshsvc@users.noreply.github.com>
2022-08-16 17:41:30 +0530

    CH-338: Initial check-in creating provisioner folder structure and co… (#31)
    * CH-338: Initial check-in creating provisioner folder structure and config files for install and config operations in node preparation and customer deployment phases.
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Changes after nodeprep automation using the freamework.
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Changes to framework for nodeprep
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Corrected the methods in cmdconfig and resource
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Added get_resource_cmd for all types of commands
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Staging framework code only for merge.
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Staged out siteconfig and unit test files from framework checkin
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Staged out config.py not needed in framework
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Undo const.py changes
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Undo const.py changes
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Added config.py stubs for dependency in resource.py
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Corrected typo for cotx->cortx
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Fixed codacy issues
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Codacy issue fixes
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing review comments
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing review comments
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing review comments
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing codacy
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    Signed-off-by: Rajnish-TheGreat <rajnishkumar.mishra@seagate.com>

---
+++ provisioner/const.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com or cortx-questions@seagate.com.
+
+from enum import Enum
+
+class FileType(Enum):
+    XLSX = 1
+    CSV = 2
+    INI = 3
+
+class ResourceType(Enum):
+    SERVER_NIC = 1
+    SERVER_HBA = 2
+    CLUSTER = 3
+    SOFTWARE = 4
+
+class SOFTWARE(Enum):
+    ANSIBLE = 1
+    NODECLI = 2
+    K8S_CRI = 3
+
