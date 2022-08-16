7aae5156e8ba6c67e4b10dce8e7b595388e1b105
shaileshsvc <92013295+shaileshsvc@users.noreply.github.com>
2022-08-16 17:41:30 +0530

    CH-338: Initial check-in creating provisioner folder structure and co… (#31)
    * CH-338: Initial check-in creating provisioner folder structure and config files for install and config operations in node preparation and customer deployment phases.
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Changes after nodeprep automation using the freamework.
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Changes to framework for nodeprep
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Corrected the methods in cmdconfig and resource
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Added get_resource_cmd for all types of commands
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Staging framework code only for merge.
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Staged out siteconfig and unit test files from framework checkin
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Staged out config.py not needed in framework
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Undo const.py changes
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Undo const.py changes
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Added config.py stubs for dependency in resource.py
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Corrected typo for cotx->cortx
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Fixed codacy issues
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Codacy issue fixes
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing review comments
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing review comments
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing review comments
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    * CH-338 Addressing codacy
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>
    
    Signed-off-by: shaileshsvc <shailesh.v.chaudhari@seagate.com>

---
+++ provisioner/config.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+# For any questions about this software or licensing, please email
+# opensource@seagate.com or cortx-questions@seagate.com.
+
+# Todo use cortx.utils.kv_store.kv_store_collection import YamlKvStore
+
+import yaml
+import collections
+from const import FileType, ResourceType
+from error import CustomError
+
+
+class ResourcesConfig:
+    def get_config_sections(self):
+        try:
+            with open(self.config_file, 'r') as f:
+                try:
+                    cfgdata = yaml.safe_load(f)
+                    return cfgdata
+                except Exception as e:
+                    raise CustomError(f"Unable to parse config file. Error {e}")
+        except Exception as e:
+            raise CustomError(f"Unable to parse config file. Error {e}")
+
+    def get_resources(self):
+        try:
+            return self.config_dict.keys()
+        except Exception as e:
+            raise CustomError(f"Unable to parse resources in config file. Error {e}")
+
+    def get_resource_type(self, resource_id):
+        try:
+            return self.config_dict[resource_id]['resource_type']
+        except Exception as e:
+            raise CustomError(f"Unable to parse resource type in config file. Error {e}")
+
+    def get_resource_cmd(self, resource_id, cmd_type):
+        try:
+            return self.config_dict[resource_id][cmd_type] if cmd_type in self.config_dict[resource_id] else None
+        except Exception as e:
+            raise CustomError(f"Unable to parse resource command in config file. Error {e}")
+
+    def __init__(self, file_type: FileType, config_file: str):
+        try:
+            self.file_type = file_type
+            self.config_file = config_file
+            self.config_dict = self.get_config_sections()
+        except Exception as e:
+            raise CustomError(f"Unable to Open config file. Error {e}")
+
+
+class SiteConfig:
+    def __init__(self, file_type: FileType, config_file: str):
+        self.file_type = file_type
+        self.config_file = config_file
+    def create_deployment_config(self) -> bool:
+        ''' read input config (XLSX, site_survey_document.xlsx) into deployment_config yaml object '''
+        self.resource_config = collections.defaultdict()
+        return True
+    def validate_deployment_config(self) -> bool:
+        ''' validate deployment_config for expected configuration format and / or values '''
+        return True
+    def get_resource_config(self, resource_type: ResourceType, resource_id: str) -> dict:
+        ''' read and return config json or yaml subset for given respurce type and id '''
+        return self.resource_config
+
+
+if __name__ == "__main__":
+    cfg = ResourcesConfig(FileType.INI, './config/haloprov.yaml')
+    print(cfg.get_resources())
+
+
