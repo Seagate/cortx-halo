1d2c7a49cba7d52430dcbba63296bfdeb3eaa7e9
Yash Khadse <yash.d.khadse@seagate.com>
2022-08-17 05:46:06 -0600

    Create Vue SFCs for Users and Tenant
    Signed-off-by: Yash Khadse <yash.d.khadse@seagate.com>

---
+++ manager/gui/src/components/users/LrTenant.vue
@@ -0,0 +1,82 @@
+<!--
+* CORTX-CSM: CORTX Management web.
+* Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Affero General Public License for more details.
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+* For any questions about this software or licensing,
+* please email opensource@seagate.com.
+-->
+<template>
+  <div class="manage-user-page-container users-page">
+    <div class="page-title">
+        Tenant
+    </div>
+    <v-divider></v-divider>
+    <div class="pt-5">
+      <SgtTabs :tabsInfo="tabsInfo" @selected="onTabChange" />
+    <component :is="activeTab" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import SgtTabs from "../../lib/components/SgtTabs/SgtTabs.vue";
+import { TabsInfo } from "../../lib/components/SgtTabs/SgtTabs.model";
+import LrTenantSetup from "./LrTenantSetup.vue"
+import LrTenantSecurity from "./LrTenantSecurity.vue"
+@Component({
+  name: "LrTenant",
+  components: { SgtTabs,LrTenantSetup,LrTenantSecurity},
+})
+export default class LrTenant extends Vue {
+  public tabsInfo: TabsInfo = [
+    {
+      id: 1,
+      label: "Setup",
+    },
+    {
+      id: 2,
+      label: "Security",
+    },
+    {
+      id: 3,
+      label: "Audit Log",
+    },
+    {
+      id: 4,
+      label: "Monitoring",
+    }
+  ];
+
+  activeTab: string = "LrTenantSetup";
+
+  public onTabChange(value: number) {
+    switch (value) {
+      case 1:
+        this.activeTab = "LrTenantSetup";
+        break;
+      case 2:
+        this.activeTab = "LrTenantSecurity";
+        break;
+      case 3:
+        this.activeTab = "";
+        break;
+      case 4:
+        this.activeTab = "";
+        break;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
