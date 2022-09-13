12336021b48c37b3fef0f040c9f91296ae200392
Yash Khadse <yash.d.khadse@seagate.com>
2022-09-12 21:59:32 -0600

    Will be rebased
    Signed-off-by: Yash Khadse <yash.d.khadse@seagate.com>

--- manager/gui/src/lib/tests/unit/SgtCard/SgtToggle.spec.ts
+++ manager/gui/src/lib/tests/unit/SgtCard/SgtToggle.spec.ts
@@ -8,7 +8,7 @@ describe('lib-SgtToggle.vue', () => {
     const localVue = createLocalVue();
     let vuetify: any;
     let wrapper: any;
-    let toggleState: boolean = false;
+    const toggleState: boolean = false;
 
     beforeEach(() => {
         vuetify = new Vuetify()
@@ -40,11 +40,6 @@ describe('lib-SgtToggle.vue', () => {
         expect(toggleContainer.exists()).toBe(true);
     })
 
-    it('Check if toggle button exists', async () => {
-        const toggleContainer = wrapper.find(".toggle-container");
-        expect(toggleContainer.exists()).toBe(true);
-    })
-
     it('Check if toggle is off by default', async () => {
         expect(wrapper.vm.value).toBe(false);
     })
@@ -52,8 +47,23 @@ describe('lib-SgtToggle.vue', () => {
     it('Check if toggle is working on click', async () => {
         const toggleContainer = wrapper.find(".toggle-container");
         await toggleContainer.trigger('click');
-        console.log(toggleContainer.classes());
         expect(wrapper.emitted().change.length).toBe(1);
         expect(wrapper.emitted().change[0][0]).toBe(true);
+        
+        await wrapper.setData({value: true});
+        await toggleContainer.trigger('click');
+        expect(wrapper.emitted().change.length).toBe(2);
+        expect(wrapper.emitted().change[1][0]).toBe(false);
+    })
+    
+    it('Check the existence of tooltip', async () => {
+        const vt = wrapper.findComponent({ name: 'v-tooltip' });
+        
+        expect(vt.props().disabled).toBe(false);
+        expect(vt.exists()).toBe(true);
+    })
+
+    it('Check if the tooltip text is correct', async () => {
+        expect(wrapper.props().tooltip).toBe("Test Off");
     })
 })
